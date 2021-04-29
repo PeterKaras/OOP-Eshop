be5a58245a7d43d508a09315e85589ac2a5cd8da
Tomáš Kováčik <kovacik@netgrif.com>
2021-04-29 15:37:05 +0200

    initial commit

---
+++ src/test/java/sk/stuba/fei/uim/oop/assignment3/Assignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop.assignment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

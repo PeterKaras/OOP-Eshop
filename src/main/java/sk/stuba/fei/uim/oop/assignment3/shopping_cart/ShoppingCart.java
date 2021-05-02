5a4741ea502ee23febc36e085f97f5f4f48dda8e
Jakub Kovář <jakub.kovar@stuba.sk>
2021-05-02 13:07:45 +0200

    removed User related implementation

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/shopping_cart/ShoppingCart.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/shopping_cart/ShoppingCart.java
@@ -1,7 +1,6 @@
 package sk.stuba.fei.uim.oop.assignment3.shopping_cart;
 
 import lombok.Data;
-import sk.stuba.fei.uim.oop.assignment3.user.User;
 
 import javax.persistence.*;
 import java.util.List;
@@ -13,9 +12,6 @@ public class ShoppingCart {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    @ManyToOne
-    private User user;
 
     @OneToMany(orphanRemoval = true)
     private List<CartEntry> shoppingList;
